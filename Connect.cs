d9cc4f3f7036073f4e65f5cd8e67d4422f75b808
Shauren <shauren.trinity@gmail.com>
2015-07-21 18:04:15 +0200

    Added vsix extension for Visual Studio 2015

--- Connect.cs
+++ Connect.cs
@@ -1,4 +1,4 @@
-﻿using EnvDTE;
+using EnvDTE;
 using EnvDTE80;
 using Extensibility;
 using System;
--- WhitespaceCleanerExtension/Properties/AssemblyInfo.cs
+++ WhitespaceCleanerExtension/Properties/AssemblyInfo.cs
@@ -5,22 +5,19 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Shared")]
+[assembly: AssemblyTitle("WhitespaceCleanerExtension")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Shared")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("WhitespaceCleanerExtension")]
+[assembly: AssemblyCopyright("shauren.dev@gmail.com")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("25b65718-14c0-4a62-8884-3c61de51f9d9")]
+[assembly: ComVisible(true)]
 
 // Version information for an assembly consists of the following four values:
 //
